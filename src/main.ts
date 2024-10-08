51f0232cf7f23448eb46f64ccfb2d609389cc28f
Guangyang Chen <gchen79@ucsc.edu>
2024-10-07 21:40:36 -0700

    Step 2 completed

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// Step 1: A button you can click
 const button = document.createElement("button");
 button.innerHTML = "Click Me 😊";
 app.append(button);
+
+// Step 2: Clicking increases a counter
+let counter: number = 0;
+
+const counterDiv = document.createElement("div");
+counterDiv.innerHTML = `${counter} smiles`;
+app.append(counterDiv);
+
+button.addEventListener("click", () => {
+    counter++;
+    counterDiv.innerHTML = `${counter} smiles`;
+});
